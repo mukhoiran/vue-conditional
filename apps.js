de554c517a1670694917e160d76c95cf1f1ebb2f
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-07 18:38:22 +0700

    v-if v-else

--- apps.js
+++ apps.js
@@ -1,8 +1,9 @@
 var app = new Vue({
   el:'#app',
   data:{
-    // show:false,
-    show:'exist',
-    message: 'Learning conditional'
+    show:false,
+    // show:'exist',
+    message: 'Learning conditional',
+    message2: 'Not learning conditional'
   }
 })
